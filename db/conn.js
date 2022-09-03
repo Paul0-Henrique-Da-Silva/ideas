c23f432f1978f3f7e444305520fed097cd3cbdf6
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-09-03 12:26:27 -0300

    importando core_modules, configurado sequelize

---
+++ db/conn.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require('sequelize')
+
+const sequelize = new Sequelize('ideas', 'root', '9383',{
+ host: 'localhost',
+ dialect: 'mysql'
+}) 
+
+try {
+    sequelize.authenticate()
+    console.log('Conectado ao Banco')
+} catch (error) {
+    console.log('Erro ao conectar ao Banco', error)
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+const app = require('express')()
+const flash = require('express-flash')
+const exphbs = require('express-handlebars')
+const session = require('express-session')
+const fileStore = require('session-file-store')
+
+

d8bc268f9d129c53b0be751c5b94bc893419e33a
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-09-04 17:48:17 -0300

    configurando em index, view engine, message flash, session - criado page main

--- index.js
+++ index.js
@@ -1,10 +1,60 @@
-const app = require('express')()
 const flash = require('express-flash')
 const exphbs = require('express-handlebars')
+const express = require('express')
 const session = require('express-session')
-const fileStore = require('session-file-store')(session)
+const FileStore = require('session-file-store')(session)
 
 const conn = require('./db/conn')
+
+const app = express()
+
+// Templete engine
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+// receber resposta do bory
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+//receber dado em json
+app.use(express.json())
+
+//session  middleware , onde express vai salvar a sessoes
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: function() {},
+            path: require('path').join(require('os').tmpdir(), 'sessions'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+)
+
+//flash message
+app.use(flash())
+
+// arquivo publicos
+app.use(express.static('public'))
+
+// configurar a sesseion para res
+app.use((resquest, response, next) =>{
+    if(resquest.session.userid){
+        response.locals.session = request.session }
+    next()    
+})
+
 
 conn.sync().then(() => app.listen(3000)
 ).catch((Error) => console.log(Error))

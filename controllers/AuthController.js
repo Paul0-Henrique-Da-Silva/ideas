8ed8dc98af83d39d2f42e19d85fe5613ce92a94e
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-09-05 14:03:59 -0300

    tratando  authC/authRoutes passwoer e confirmpass , flas message css/html/main

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,9 +1,25 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = class  AuthControler {
-    static login(request, response) {
+    static login(_request, response) {
         response.render('auth/login')
     }
 
-    static register(request, response) {
+    static register(_request, response) {
         response.render('auth/register')
+    }
+
+    static async registerPost(request, response) {
+        const { name, email, password, confirmpassword } = request.body
+        
+        //password match validation
+        if(password != confirmpassword) {
+            // mensagem
+            request.flash('message', 'As senhas não conferem, tente novamente')
+            response.render('auth/register')
+            return
+        }
     }
 }

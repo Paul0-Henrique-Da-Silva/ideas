ce8439275b37adb1a776568a0b3f29c56271c65d
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-09-05 15:22:44 -0300

    usando bcrypt em password - inicianmente passando email e password para User/dada - criando session ao logar - mais dinamica page main em seu menu(nav)

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -13,13 +13,35 @@ module.exports = class  AuthControler {
 
     static async registerPost(request, response) {
         const { name, email, password, confirmpassword } = request.body
+         
+        //checar se usúario existe
+        const checkIfUserExists = await User.findOne({where: {email: email}})
+        if(checkIfUserExists) { 
+            request.flash('message', 'O e-mail já está em uso!')
+            response.render('auth/register')
+            return }
         
         //password match validation
+        // mensagem
         if(password != confirmpassword) {
-            // mensagem
             request.flash('message', 'As senhas não conferem, tente novamente')
             response.render('auth/register')
-            return
+            return }
+        // creaar password
+        const salt = bcrypt.genSaltSync(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        const user = { name, email, password: hashedPassword}
+        try {
+            const  createdUser = await User.create(user)
+            request.session.userid = createdUser.id // inicializando session
+            request.flash('message', 'Cadastro realizado com sucesso!')
+            request.session.save(() => { 
+                response.redirect('/')
+            })
+        } catch (error) {
+            console.log(error)   
         }
+            
+
     }
 }
